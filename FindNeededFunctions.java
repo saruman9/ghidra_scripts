f55da3ff3d32d3542caaeb2558d84cbd7d4abca3
saruman9 <rum.274.4@gmail.com>
2019-07-11 17:44:32 +0300

    [FD_FUNCS] Add filters, output to CSV format

--- FindNeededFunctions.java
+++ FindNeededFunctions.java
@@ -5,14 +5,17 @@
 //@menupath
 //@toolbar
 
+import generic.stl.Pair;
 import ghidra.app.decompiler.DecompInterface;
 import ghidra.app.decompiler.DecompileException;
 import ghidra.app.decompiler.DecompileOptions;
 import ghidra.app.decompiler.DecompileResults;
 import ghidra.app.script.GhidraScript;
+import ghidra.program.model.address.Address;
 import ghidra.program.model.listing.Function;
 import ghidra.program.model.listing.FunctionIterator;
 import ghidra.program.model.listing.FunctionManager;
+import ghidra.program.model.mem.MemoryAccessException;
 import ghidra.program.model.pcode.HighFunction;
 import ghidra.program.model.pcode.HighParam;
 import ghidra.program.model.pcode.HighVariable;
@@ -23,9 +26,11 @@ import ghidra.util.Msg;
 import ghidra.util.exception.NotYetImplementedException;
 import ghidra.util.task.TaskMonitor;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class FindNeededFunctions extends GhidraScript {
+
     @Override
     protected void run() throws Exception {
         if (currentProgram == null) {
@@ -48,24 +53,57 @@ public class FindNeededFunctions extends GhidraScript {
         }
 
         Function currentFunction = functionManager.getFunctionContaining(currentAddress);
-//        processFunction(ifc, currentFunction);
+//        printFunction(ifc, currentFunction, monitor);
+//        processFunction(ifc, currentFunction, monitor);
+//        return;
+
+        printf("NmFun, AdrFunHex, AdrFunDec, SzFun, StOffSrc, StOffDst\n");
 
         while (functionIterator.hasNext()) {
             Function function = functionIterator.next();
-            if (function.getParameterCount() > 0) {
+            if (function.getParameterCount() > 0 && isFoundNeededBytes(function)) {
                 processFunction(ifc, function, monitor);
                 if (monitor.isCancelled()) {
                     return;
                 }
             }
         }
+    }
 
+
+    private boolean isFoundNeededBytes(Function function) {
+        Address addressBytes = function.getEntryPoint().subtract(4);
+        byte[] standardBytes = new byte[]{(byte) 0xF3, 0x0F, 0x1E, (byte) 0xFB};
+        try {
+            byte[] bytes = getBytes(addressBytes, 4);
+            return Arrays.equals(bytes, standardBytes);
+        } catch (MemoryAccessException e) {
+            return false;
+        }
+    }
+
+    private void printFunction(DecompInterface ifc, Function function, TaskMonitor monitor) {
+        HighFunction highFunction = getHighFunction(ifc, function);
+        if (highFunction == null) {
+            return;
+        }
+
+        Iterator<PcodeOpAST> opASTIterator = highFunction.getPcodeOps();
+        while (opASTIterator.hasNext()) {
+            PcodeOpAST op = opASTIterator.next();
+            if (op != null) {
+                printf("%s\n", op);
+            }
+        }
     }
 
     private void processFunction(DecompInterface ifc, Function function, TaskMonitor monitor) {
 //        printf("processing %s : %s\n", function.getName(), function.getEntryPoint());
 
         HighFunction high = getHighFunction(ifc, function);
+        if (high == null) {
+            return;
+        }
 
         Iterator<PcodeOpAST> opASTIterator = high.getPcodeOps();
         PcodeOpAST storeOp = null;
@@ -74,16 +112,34 @@ public class FindNeededFunctions extends GhidraScript {
             if (op != null) {
 //                printf("%s\n", op);
                 if (op.getOpcode() == PcodeOp.STORE) {
-                    Varnode storeVarnode = op.getInput(1);
-                    Varnode varnode = findRootOfVarnode(storeVarnode, monitor);
-                    if (monitor.isCancelled()) {
+                    Varnode storeDstVarnode = op.getInput(1);
+                    Varnode storeSrcVarnode = op.getInput(2);
+                    try {
+                        Pair<Varnode, Integer> varnodeDst = findRootOfVarnode(storeDstVarnode, 0, monitor);
+                        Pair<Varnode, Integer> varnodeSrc = findRootOfSrcVarnode(storeSrcVarnode, false, 0, monitor);
+                        if (monitor.isCancelled()) {
+                            return;
+                        }
+                        if (varnodeDst != null && varnodeSrc != null) {
+//                            HighVariable highVariable = varnode.getHigh();
+                            int stackOffsetDst = varnodeDst.first.getHigh().getStorage().getStackOffset();
+                            int stackOffsetSrc = varnodeSrc.first.getHigh().getStorage().getStackOffset();
+//                            if ((stackOffsetDst == 0x4 || stackOffsetDst == 0x10) && (stackOffsetSrc == 0x4 || stackOffsetSrc == 0x10)) {
+                            printf("%s, 0x%x, %d, %d, 0x%x, 0x%x\n",
+                                    function.getName(),
+                                    function.getEntryPoint().getOffset(),
+                                    function.getEntryPoint().getOffset(),
+                                    function.getBody().getNumAddresses(),
+                                    stackOffsetSrc,
+                                    stackOffsetDst);
+//                            }
+//                            printf("%s, %s\n", highVariable, highVariable.getStorage().getStackOffset());
+//                            return;
+                        }
+                    } catch (StackOverflowError e) {
+//                        printf("WARNING! StackOverflowError\n");
                         return;
                     }
-                    if (varnode != null) {
-                        printf("Function: %s ( %s ), %s\n", function.getName(), function.getEntryPoint(), varnode);
-                        return;
-                    }
-
                 }
             }
         }
@@ -93,21 +149,22 @@ public class FindNeededFunctions extends GhidraScript {
         DecompileResults res = ifc.decompileFunction(func, 300, null);
         HighFunction high = res.getHighFunction();
         if (high == null) {
-            printf("\nWARNING: %s returned null HighFunction\n", func);
+            printf("\nWARNING: %s ( %s ) returned null HighFunction\n", func.getName(), func.getEntryPoint());
         }
         return high;
     }
 
-    private Varnode findRootOfVarnode(Varnode varnode, TaskMonitor monitor) {
+    private Pair<Varnode, Integer> findRootOfVarnode(Varnode varnode, int jumps, TaskMonitor monitor) {
         if (monitor.isCancelled()) {
             return null;
         }
         HighVariable highVariable = varnode.getHigh();
         if (highVariable instanceof HighParam) {
 //            printf("\nIt's param! %s\n", highVariable);
-            return varnode;
+            return new Pair<>(varnode, jumps);
         }
         PcodeOp pcodeOp = varnode.getDef();
+//        printf("DEF (%s): %s\n", varnode, pcodeOp);
         if (pcodeOp == null) {
 //            printf("WARNING! Pcode NULL. %s\n", varnode);
 //            return varnode;
@@ -117,27 +174,106 @@ public class FindNeededFunctions extends GhidraScript {
         switch (opcode) {
             case PcodeOp.STORE:
             case PcodeOp.LOAD: {
-                return findRootOfVarnode(pcodeOp.getInput(1), monitor);
+                return findRootOfVarnode(pcodeOp.getInput(1), jumps + 1, monitor);
             }
-            case PcodeOp.INT_MULT:
-            case PcodeOp.INDIRECT:
-            case PcodeOp.MULTIEQUAL:
-            case PcodeOp.INT_NEGATE:
-            case PcodeOp.INT_ZEXT:
-            case PcodeOp.INT_SEXT:
-            case PcodeOp.INT_RIGHT:
-            case PcodeOp.INT_LEFT:
-            case PcodeOp.INT_SUB:
-            case PcodeOp.INT_XOR:
-            case PcodeOp.INT_AND:
-            case PcodeOp.INT_OR:
+            case PcodeOp.BOOL_AND:
+            case PcodeOp.BOOL_OR:
+            case PcodeOp.BOOL_XOR:
             case PcodeOp.CAST:
             case PcodeOp.COPY:
+            case PcodeOp.INDIRECT:
+            case PcodeOp.INT_2COMP:
+            case PcodeOp.INT_ADD:
+            case PcodeOp.INT_AND:
+            case PcodeOp.INT_DIV:
+            case PcodeOp.INT_EQUAL:
+            case PcodeOp.INT_LEFT:
+            case PcodeOp.INT_LESS:
+            case PcodeOp.INT_MULT:
+            case PcodeOp.INT_NEGATE:
+            case PcodeOp.INT_NOTEQUAL:
+            case PcodeOp.INT_OR:
+            case PcodeOp.INT_REM:
+            case PcodeOp.INT_RIGHT:
+            case PcodeOp.INT_SEXT:
+            case PcodeOp.INT_SUB:
+            case PcodeOp.INT_XOR:
+            case PcodeOp.INT_ZEXT:
+            case PcodeOp.MULTIEQUAL:
+            case PcodeOp.PIECE:
             case PcodeOp.PTRADD:
             case PcodeOp.PTRSUB:
-            case PcodeOp.INT_ADD: {
-                return findRootOfVarnode(pcodeOp.getInput(0), monitor);
+            case PcodeOp.SUBPIECE: {
+                return findRootOfVarnode(pcodeOp.getInput(0), jumps + 1, monitor);
             }
+            case PcodeOp.CALLIND:
+            case PcodeOp.CALL: {
+//                printf("WARNING! Pcode CALL\n");
+                return null;
+            }
+
+            default:
+                throw new NotYetImplementedException("Support for PcodeOp " + pcodeOp.toString() + " not implemented");
+        }
+    }
+
+    private Pair<Varnode, Integer> findRootOfSrcVarnode(Varnode varnode, boolean isDereferenced, int jumps, TaskMonitor monitor) {
+        if (monitor.isCancelled()) {
+            return null;
+        }
+        HighVariable highVariable = varnode.getHigh();
+        if (highVariable instanceof HighParam) {
+//            printf("\nIt's param! %s\n", highVariable);
+            if (isDereferenced) {
+                return new Pair<>(varnode, jumps);
+            } else {
+                return null;
+            }
+        }
+        PcodeOp pcodeOp = varnode.getDef();
+//        printf("DEF (%s): %s\n", varnode, pcodeOp);
+        if (pcodeOp == null) {
+//            printf("WARNING! Pcode NULL. %s\n", varnode);
+//            return varnode;
+            return null;
+        }
+        int opcode = pcodeOp.getOpcode();
+        switch (opcode) {
+            case PcodeOp.STORE:
+            case PcodeOp.LOAD: {
+                return findRootOfSrcVarnode(pcodeOp.getInput(1), true, jumps + 1, monitor);
+            }
+            case PcodeOp.BOOL_AND:
+            case PcodeOp.BOOL_OR:
+            case PcodeOp.BOOL_XOR:
+            case PcodeOp.CAST:
+            case PcodeOp.COPY:
+            case PcodeOp.INDIRECT:
+            case PcodeOp.INT_2COMP:
+            case PcodeOp.INT_ADD:
+            case PcodeOp.INT_AND:
+            case PcodeOp.INT_DIV:
+            case PcodeOp.INT_EQUAL:
+            case PcodeOp.INT_LEFT:
+            case PcodeOp.INT_LESS:
+            case PcodeOp.INT_MULT:
+            case PcodeOp.INT_NEGATE:
+            case PcodeOp.INT_NOTEQUAL:
+            case PcodeOp.INT_OR:
+            case PcodeOp.INT_REM:
+            case PcodeOp.INT_RIGHT:
+            case PcodeOp.INT_SEXT:
+            case PcodeOp.INT_SUB:
+            case PcodeOp.INT_XOR:
+            case PcodeOp.INT_ZEXT:
+            case PcodeOp.MULTIEQUAL:
+            case PcodeOp.PIECE:
+            case PcodeOp.PTRADD:
+            case PcodeOp.PTRSUB:
+            case PcodeOp.SUBPIECE: {
+                return findRootOfSrcVarnode(pcodeOp.getInput(0), isDereferenced, jumps + 1, monitor);
+            }
+            case PcodeOp.CALLIND:
             case PcodeOp.CALL: {
 //                printf("WARNING! Pcode CALL\n");
                 return null;
