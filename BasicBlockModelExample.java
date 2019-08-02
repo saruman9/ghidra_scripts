16fe3eaa6047b4982f42fb87a89845b5f107212b
saruman9 <rum.274.4@gmail.com>
2019-08-02 10:53:30 +0300

    [BB] Print current BB (instructions), prev BB, next BB

--- BasicBlockModelExample.java
+++ BasicBlockModelExample.java
@@ -8,7 +8,12 @@
 import ghidra.app.script.GhidraScript;
 import ghidra.program.model.block.BasicBlockModel;
 import ghidra.program.model.block.CodeBlock;
+import ghidra.program.model.block.CodeBlockIterator;
+import ghidra.program.model.block.CodeBlockReference;
+import ghidra.program.model.block.CodeBlockReferenceIterator;
 import ghidra.program.model.listing.CodeUnitIterator;
+import ghidra.program.model.listing.Function;
+import ghidra.program.model.listing.FunctionManager;
 import ghidra.program.model.listing.Listing;
 
 public class BasicBlockModelExample extends GhidraScript {
@@ -16,11 +21,35 @@ public class BasicBlockModelExample extends GhidraScript {
     @Override
     protected void run() throws Exception {
         BasicBlockModel basicBlockModel = new BasicBlockModel(currentProgram);
-        CodeBlock[] codeBlock = basicBlockModel.getCodeBlocksContaining(currentAddress, monitor);
-        Listing listing = currentProgram.getListing();
-        CodeUnitIterator codeUnitIterator = listing.getCodeUnits(codeBlock[0], true);
-        while (codeUnitIterator.hasNext()) {
-            printf("%s\n", codeUnitIterator.next());
+        FunctionManager functionManager = currentProgram.getFunctionManager();
+        for (Function currentFunction : functionManager.getFunctions(true)) {
+            printf("Function: %s ( %s )\n", currentFunction.getName(), currentFunction.getEntryPoint());
+            CodeBlockIterator codeBlockIterator = basicBlockModel.getCodeBlocksContaining(currentFunction.getBody(), monitor);
+            while (codeBlockIterator.hasNext()) {
+                CodeBlock codeBlock = codeBlockIterator.next();
+                printf("\n\tCode Block: %s ( %s )\n", codeBlock.getName(), codeBlock.getFirstStartAddress());
+
+                CodeBlockReferenceIterator codeBlockReferenceSourcesIterator = codeBlock.getSources(monitor);
+                printf("\t\tSources:\n");
+                while (codeBlockReferenceSourcesIterator.hasNext()) {
+                    CodeBlockReference codeBlockReference = codeBlockReferenceSourcesIterator.next();
+                    CodeBlock codeBlockSource = codeBlockReference.getSourceBlock();
+                    printf("\t\t%s ( %s )\n", codeBlockSource.getName(), codeBlockSource.getFirstStartAddress());
+                }
+                CodeBlockReferenceIterator codeBlockReferenceDestsIterator = codeBlock.getDestinations(monitor);
+                printf("\n\t\tDestinations:\n");
+                while (codeBlockReferenceDestsIterator.hasNext()) {
+                    CodeBlockReference codeBlockReference = codeBlockReferenceDestsIterator.next();
+                    CodeBlock codeBlockDest = codeBlockReference.getDestinationBlock();
+                    printf("\t\t%s ( %s )\n", codeBlockDest.getName(), codeBlockDest.getFirstStartAddress());
+                }
+
+                Listing listing = currentProgram.getListing();
+                CodeUnitIterator codeUnitIterator = listing.getCodeUnits(codeBlock, true);
+                while (codeUnitIterator.hasNext()) {
+                    printf("\t%s\n", codeUnitIterator.next());
+                }
+            }
         }
     }
 }
